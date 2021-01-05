110dc0d719bb31c903e5d1e3e9010cb1bbfba662
leo123nunes <ti.userleo@hotmail.com>
2021-01-05 10:55:40 -0300

    Creating the file input for the subtitles, and styling the word's amount's appearance

---
+++ src/components/Home.vue
@@ -0,0 +1,50 @@
+<template>
+    <v-container>
+        <v-form>
+            <v-file-input label="Select the subtitle" 
+            multiples 
+            prepend-icon="mdi-message-text"
+            append-icon="mdi-send"
+            v-model="files"
+            chips
+            outlined
+            @click:append="loadSubtitles"
+            />
+        </v-form>
+    <div class="pills">
+        <Pill v-for="word in groupedWords" :key="word.name" :name="word.name"
+        :amount="word.amount"></Pill>
+    </div>
+    </v-container> 
+</template>
+
+<script>
+import Pill from "./Pill.vue";
+export default {
+  components: { Pill },
+  data: function () {
+    return {
+      files: [],
+      groupedWords: [
+        { name: "words", amount: "1234" },
+        { name: "test", amount: "900" }
+      ],
+    };
+  },
+  methods: {
+      loadSubtitles(){
+          console.log(this.files)
+      }
+  }
+};
+</script>
+
+<style scoped>
+    .pills{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        /* flex-direction: space-around; */
+        flex-wrap: wrap;
+    }
+</style>
---
+++ src/components/Pill.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="pill">
+    <span>{{ name }}</span>
+    <span class="amountArea">{{ amount }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["name", "amount"],
+};
+</script>
+
+<style>
+.pill *{
+    padding: 5px;
+}
+
+.pill {
+  margin: 5px;
+  background-color: rgb(187, 52, 52);
+  border-radius: 18px;
+  padding: 4px 8px;
+  color: #fff;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
+.amountArea {
+  height: 30px;  
+  width: 30px;      
+  border-radius: 18px;
+  background-color: #fff;
+  color: rgb(253, 79, 79);
+  font-size: 0.7em;
+  /* padding: 5px; */
+  font-weight: bolder;
+  /* margin-left: 5px; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
